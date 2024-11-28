5afbb2d693f0a0035649c91d7df2788b721c7a17
tvantard <t.vantard@univ-lyon2.fr>
2024-11-28 17:28:05 +0100

    Add files via upload

---
+++ Projet/Classes_Et_Outils/Author.py
@@ -0,0 +1,26 @@
+class Author:
+    def __init__(self, name, production):
+        self.__name = name
+        self.__ndoc = len(production)
+        self.__production = production
+    
+    def __str__(self):
+        return self.__name
+    
+    def addDoc(self, document):
+        if (document.getAut()==self.__str__()):
+            self.__production.append(document)
+            self.__ndoc=self.__ndoc+1
+        else :
+            print("Ceci n'est pas votre document")
+
+    def addListDoc(self, documents):
+        for i in documents:
+            self.addDoc(i)
+
+    def printAut(self):
+       print("Informations complètes de l'auteur :")
+       print(f"Nom : {self.__name}")
+       print(f"Nombre de documents : {self.__ndoc}")
+       for i in self.__production:
+           print("\t" + i.__str__())
